1e7a914e21ec756c9a4d899ad1be94158a759c4f
mattialicausi <licausivitomattia@gmail.com>
2023-01-09 23:43:27 +0100

    aggiunto jumbo e navbar in header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,36 @@
+
+<header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary container">
+        <div class="container-fluid p-0">
+          <a class="navbar-brand text-uppercase fw-bold" href="#">lawrence</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+
+
+            <ul class="navbar-nav me-5">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Reservation</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Menu</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">About</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Contact</a>
+              </li>
+            </ul>
+
+            <button class="my-btn" type="submit">Order Now</button>
+
+        </div>
+      </nav>
+
+</header>
---
+++ resources/views/partials/jumbo.blade.php
@@ -0,0 +1,36 @@
+ <div id="carouselExample" class="carousel slide">
+<div class="row">
+    <div class="col-10">
+        <div class="carousel-inner" id="my-carousel">
+            <div class="carousel-item container-img active">
+              <img src="{{Vite::asset('resources/img/jumbo1.jpg')}}" class="d-block my-img" alt="...">
+            </div>
+            <div class="carousel-item container-img">
+              <img src="{{Vite::asset('resources/img/carousel6.jpeg')}}" class="d-block my-img" alt="...">
+            </div>
+            <div class="carousel-item container-img">
+              <img src="{{Vite::asset('resources/img/carousel5.jpeg')}}" class="d-block my-img" alt="...">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+      </div>
+
+      <div class="col-2 d-flex justify-content-between align items center">
+            <div>Dal 1934</div>
+            <div class="linea"></div>
+            <div>Rue de la Bont&eacute;</div>
+      </div>
+
+    </div>
+
+</div>
+
+
+
